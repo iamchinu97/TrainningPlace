13138627238dacf89b2ffbab7938385206c68bbe
iamchinu97 <shimpichinmay@gmail.com>
2019-07-09 17:12:01 +0530

    Added assignments for week2 day1 and day2

---
+++ Week2/DayOne/my-maven-project/src/test/java/com/sapient/demo/EmployeeTest.java
@@ -0,0 +1,43 @@
+package com.sapient.week1.assessment;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EmployeeTest {
+
+	@Test
+	public void testForReadData() throws FileNotFoundException, IOException {
+		EmployeeDAO employee= new EmployeeDAO();
+		List<EmployeeBean> list=new ArrayList<EmployeeBean>();
+		list.add(new EmployeeBean(18,"Rajat",50000));
+		list.add(new EmployeeBean(21,"Chinmay",60000));
+		list.add(new EmployeeBean(19,"Ajay",20000));
+		list.add(new EmployeeBean(57, "Gurpal", 60000));
+		Assert.assertArrayEquals(list.toArray(),employee.readData().toArray());
+	}
+	@Test
+	public void testforGetEmployee() throws FileNotFoundException, IOException
+	{
+		EmployeeDAO employee= new EmployeeDAO();
+		Assert.assertEquals(new EmployeeBean(21,"Chinmay",60000),employee.getEmployee(21));
+		Assert.assertEquals(new EmployeeBean(18,"Rajat",50000),employee.getEmployee(18));
+	}
+	@Test
+	public void testforGetTotSalary() throws FileNotFoundException, IOException
+	{
+		EmployeeDAO employee= new EmployeeDAO();
+		Assert.assertEquals(190000.0, (double)employee.getTotSal(employee.readData()), 0.0000);
+	}
+	@Test
+	public void testforGetCount() throws FileNotFoundException, IOException
+	{
+		EmployeeDAO employee= new EmployeeDAO();
+		Assert.assertEquals(2,employee.getCount(employee.readData(), 60000.0f));
+	}
+
+}
